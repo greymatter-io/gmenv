2fc30fcdae167c33e87bfa98d07ef147f9029be2
Chris Smith <chris.b.smith@gmail.com>
2020-05-19 09:08:09 -0400

    remove dependency on bash4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.0 (May 2020)
+
+* Remove the dependency on bash4 for macOS
+
 ## 0.1.0 (May 2020)
 
 * Initial development of gmenv
--- test/test_install_and_use.sh
+++ test/test_install_and_use.sh
@@ -73,40 +73,49 @@ declare -a errors=();
 
 log 'info' '### Test Suite: Install and Use'
 
-declare -A string_tests=();
+tests__desc=(
+  'latest version'
+  'latest possibly-unstable version'
+  'latest version matching regex'
+  'specific version'
+);
 
-string_tests['latest version']="$(gmenv list-remote | grep -e "^[0-9]\+\.[0-9]\+\.[0-9]\+$" | head -n 1),latest";
-string_tests['latest possibly-unstable version']="$(gmenv list-remote | head -n 1),latest:";
-string_tests['latest version matching regex']='1.3.0,latest:^1.3';
-string_tests['specific version']="1.2.0,1.2.0";
+tests__kv=(
+  "$(gmenv list-remote | grep -e "^[0-9]\+\.[0-9]\+\.[0-9]\+$" | head -n 1),latest"
+  "$(gmenv list-remote | head -n 1),latest:"
+  '1.3.0,latest:^1.3'
+  "1.2.0,1.2.0"
+)
 
-declare kv k v;
-declare -i test_num=1;
+tests_count=${#tests__desc[@]}
 
-for desc in "${!string_tests[@]}"; do
+declare desc kv k v;
+
+for ((test_num=0; test_num<${tests_count}; ++test_num )) ; do
   cleanup || log 'error' 'Cleanup failed?!';
-  kv="${string_tests[${desc}]}";
+  desc=${tests__desc[${test_num}]}
+  kv="${tests__kv[${test_num}]}";
   v="${kv%,*}";
   k="${kv##*,}";
-  log 'info' "## Param Test ${test_num}/${#string_tests[*]}: ${desc} ( ${k} / ${v} )";
+  log 'info' "## Param Test ${test_num}/${tests_count}: ${desc} ( ${k} / ${v} )";
   test_install_and_use "${v}" "${k}" \
-    && log info "## Param Test ${test_num}/${#string_tests[*]}: ${desc} ( ${k} / ${v} ) succeeded" \
-    || error_and_proceed "## Param Test ${test_num}/${#string_tests[*]}: ${desc} ( ${k} / ${v} ) failed";
+    && log info "## Param Test ${test_num}/${tests_count}: ${desc} ( ${k} / ${v} ) succeeded" \
+    || error_and_proceed "## Param Test ${test_num}/${tests_count}: ${desc} ( ${k} / ${v} ) failed";
   test_num+=1;
 done;
 
-test_num=1;
-for desc in "${!string_tests[@]}"; do
+for ((test_num=0; test_num<${tests_count}; ++test_num )) ; do
   cleanup || log 'error' 'Cleanup failed?!';
-  kv="${string_tests[${desc}]}";
+  desc=${tests__desc[${test_num}]}
+  kv="${tests__kv[${test_num}]}";
   v="${kv%,*}";
   k="${kv##*,}";
-  log 'info' "## ./.greymatter-version Test ${test_num}/${#string_tests[*]}: ${desc} ( ${k} / ${v} )";
+  log 'info' "## ./.greymatter-version Test ${test_num}/${tests_count}: ${desc} ( ${k} / ${v} )";
   log 'info' "Writing ${k} to ./.greymatter-version";
   echo "${k}" > ./.greymatter-version;
   test_install_and_use "${v}" \
-    && log info "## ./.greymatter-version Test ${test_num}/${#string_tests[*]}: ${desc} ( ${k} / ${v} ) succeeded" \
-    || error_and_proceed "## ./.greymatter-version Test ${test_num}/${#string_tests[*]}: ${desc} ( ${k} / ${v} ) failed";
+    && log info "## ./.greymatter-version Test ${test_num}/${tests_count}: ${desc} ( ${k} / ${v} ) succeeded" \
+    || error_and_proceed "## ./.greymatter-version Test ${test_num}/${tests_count}: ${desc} ( ${k} / ${v} ) failed";
   test_num+=1;
 done;
 
@@ -153,21 +162,24 @@ log 'info' 'Install invalid specific version';
 cleanup || log 'error' 'Cleanup failed?!';
 
 
-declare -A neg_tests=();
-neg_tests['specific version']="9.9.9";
+neg_tests__desc=(
+  'specific version'
+  'latest:word'
+);
+neg_tests__kv=(
+  '9.9.9'
+  "latest:word"
+);
+neg_tests_count=${#neg_tests__desc[@]}
 
-neg_tests['latest:word']="latest:word";
-
-test_num=1;
-
-for desc in "${!neg_tests[@]}"; do
+for ((test_num=0; test_num<${neg_tests_count}; ++test_num )) ; do
   cleanup || log 'error' 'Cleanup failed?!';
-  k="${neg_tests[${desc}]}";
+  desc=${neg_tests__desc[${test_num}]}
+  k="${neg_tests__kv[${test_num}]}";
   expected_error_message="No versions matching '${k}' found in remote";
-  log 'info' "##  Invalid Version Test ${test_num}/${#neg_tests[*]}: ${desc} ( ${k} )";
+  log 'info' "##  Invalid Version Test ${test_num}/${neg_tests_count}: ${desc} ( ${k} )";
   [ -z "$(gmenv install "${k}" 2>&1 | grep "${expected_error_message}")" ] \
     && error_and_proceed "Installing invalid version ${k}";
-  test_num+=1;
 done;
 
 if [ "${#errors[@]}" -gt 0 ]; then
